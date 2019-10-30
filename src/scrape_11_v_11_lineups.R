2806f1fa07d4b995c1ab2171a40fe2d9e35495af
RobWHickman <robwhickman@gmail.com>
2019-10-30 18:48:28 +0000

    progress on scrapping 11v11

--- src/scrape_11_v_11_lineups.R
+++ src/scrape_11_v_11_lineups.R
@@ -5,15 +5,15 @@ extract_names <- function(text) {
 
 #function to scrape the players and positions for both starters/subs (type)
 #and away/home (team)
-get_player_positions <- function(read, team, type) {
+get_player_positions <- function(read, team, type, first11) {
   player <- read %>%
     html_nodes(paste(type, team, "a")) %>%
     html_text()
-  if(length(player) == 0) player <- check_other_scrapes(read, team, type)
+  if(length(player) == 0 & first11) player <- check_other_scrapes(read, team, type, "a")
   position <- read %>%
     html_nodes(paste(type, team, ".position")) %>%
     html_text()
-  if(length(position) == 0) position <- NA
+  if(length(position) == 0 & first11) position <- check_other_scrapes(read, team, type, ".position")
 
   if(length(player) != length(position)) {
     check <- read %>%
@@ -66,13 +66,13 @@ check_players_present <- function(read, team) {
 }
 
 #function to try other nodes to scrape players
-check_other_scrapes <- function(read, team, type) {
+check_other_scrapes <- function(read, team, type, extra) {
   player <- read %>%
-    html_nodes(paste(team, ".player a")) %>%
+    html_nodes(paste(team, ".player", extra)) %>%
     html_text()
   if(length(player) == 0) {
     player <- read %>%
-      html_nodes(paste(team, ".player a")) %>%
+      html_nodes(paste(".headers+ .lineup", team, extra)) %>%
       html_text()
     if(length(player) == 0) {
       return(NA)
@@ -81,5 +81,60 @@ check_other_scrapes <- function(read, team, type) {
     }
   } else {
     return(player)
+  }
+}
+
+#get the subs and when they came on
+#merge into the df of players
+add_sub_info <- function(players, read, team) {
+  subs_on <- read %>%
+    html_nodes(paste0(team, " .substitute")) %>%
+    html_text() %>%
+    extract_names()
+  subs_off <- read %>%
+    html_nodes(paste0(team, " .substituted")) %>%
+    html_text() %>%
+    extract_names()
+  sub_time <- read %>%
+    html_nodes(paste0(".substitutions ", team, " .time")) %>%
+    html_text() %>%
+    as.numeric()
+
+  players$time_off[unlist(lapply(subs_off, grep, players$player))] <- sub_time
+  players$time_on[unlist(lapply(subs_on, grep, players$player))] <- sub_time
+  players$sub_off[unlist(lapply(subs_off, grep, players$player))] <- 1
+  players$sub_on[unlist(lapply(subs_on, grep, players$player))] <- 1
+  return(players)
+}
+
+merge_event_info <- function(players, events) {
+  if(!all(is.na(players))) {
+    events <- events[which(events$player %in% players$player),]
+    events$player <- as.character(events$player)
+    for(event_type in c("G", "Y", "R")) {
+      if(any(events$event == event_type)) {
+          players <- left_join(
+            players,
+            events %>%
+              filter(player %in% players$player) %>%
+              filter(event == event_type) %>%
+              select(player) %>%
+              table() %>%
+              as.data.frame() %>%
+              `names<-`(c("player", event_type)),
+            by = "player"
+          )
+          na <- which(is.na(players[event_type]))
+          players[event_type][na,] <- 0
+      } else {
+        merge <- data.frame(player = players$player,
+                            event_type = 0) %>%
+          `names<-`(c("player", event_type))
+        players <- left_join(players, merge)
+      }
+    }
+    return(players)
+  } else {
+    return(NA)
   }
 }
